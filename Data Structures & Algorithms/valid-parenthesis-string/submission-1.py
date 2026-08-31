3eb1d046f88cf5dec0f44bf5a69783468da9f74d
Sanjana P <sanjana.pulla06@gmail.com>
2026-08-31 19:13:17 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        memo = {}
+
+        def dfs(i, open):
+            if open < 0:
+                return False
+
+            if i == len(s):
+                return open == 0
+
+            if (i, open) in memo:
+                return memo[(i, open)]
+
+            if s[i] == '(':
+                result = dfs(i + 1, open + 1)
+
+            elif s[i] == ')':
+                result = dfs(i + 1, open - 1)
+
+            else:
+                result = (
+                    dfs(i + 1, open) or
+                    dfs(i + 1, open + 1) or
+                    dfs(i + 1, open - 1)
+                )
+
+            memo[(i, open)] = result
+            return result
+
+        return dfs(0, 0)
