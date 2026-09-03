91c229458a403b61734213bce749ccff37de5605
Sanjana P <sanjana.pulla06@gmail.com>
2026-09-03 22:58:13 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,17 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+        for i in range(1, len(intervals)):
+            previous_end = intervals[i - 1].end
+            current_start = intervals[i].start 
+            if current_start < previous_end:
+                return False
+        return True
